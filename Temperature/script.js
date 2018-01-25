3be9e2bec139ffdaa5735dc5dfca206400f98ecb
Daniela8888 <32717667+Daniela8888@users.noreply.github.com>
2018-01-25 09:56:33 +0100

    Add files via upload

--- Temperature/script.js
+++ Temperature/script.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 		var city=$("#city").val();
 		var code=$("#code").val();
 		if(city.length>1){
-			var.urllink='http://api.openweathermap.org/data/2.5/weather?q=';
+			var urllink='http://api.openweathermap.org/data/2.5/weather?q=';
 			urllink=urllink + city;
-			}
+			
 			if(code.length==2){
 				urllink=urllink+','+code;
 			}
